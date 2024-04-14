b862e9667c7317e144592b9b9f9c75dd9ddcefb9
Matthew <matthew13479@gmail.com>
2024-04-14 13:41:10 +1000

    Added Inertia Calcs to main loop

--- driver/manualTorque.cpp
+++ driver/manualTorque.cpp
@@ -71,88 +71,88 @@ double getPracTorqueFromInertia(double scaleWeight)
 int main(int argc, char **argv)
 {
     // Set up signal handler for Ctrl+C (SIGINT)
-    // std::signal(SIGINT, signalHandler);
+    std::signal(SIGINT, signalHandler);
 
-    // using namespace mjbots;
-    // // Set up controllers and transport
-    // moteus::Controller::DefaultArgProcess(argc, argv);
-    // auto transport = moteus::Controller::MakeSingletonTransport({});
+    using namespace mjbots;
+    // Set up controllers and transport
+    moteus::Controller::DefaultArgProcess(argc, argv);
+    auto transport = moteus::Controller::MakeSingletonTransport({});
 
-    // // Options for setting up controllers
-    // moteus::Controller::Options options_common;
+    // Options for setting up controllers
+    moteus::Controller::Options options_common;
 
-    // // Set position format
-    // auto &pf = options_common.position_format;
-    // pf.position = moteus::kIgnore;
-    // pf.velocity = moteus::kIgnore;
-    // pf.feedforward_torque = moteus::kFloat;
-    // pf.kp_scale = moteus::kInt8;
-    // pf.kd_scale = moteus::kInt8;
+    // Set position format
+    auto &pf = options_common.position_format;
+    pf.position = moteus::kIgnore;
+    pf.velocity = moteus::kIgnore;
+    pf.feedforward_torque = moteus::kFloat;
+    pf.kp_scale = moteus::kInt8;
+    pf.kd_scale = moteus::kInt8;
 
-    // // Create two controllers
-    // std::vector<std::shared_ptr<moteus::Controller>> controllers = {
-    //     std::make_shared<moteus::Controller>([&]()
-    //                                          {
-    //         auto options = options_common;
-    //         options.id = 1;
-    //         return options; }()),
-    //     std::make_shared<moteus::Controller>([&]()
-    //                                          {
-    //         auto options = options_common;
-    //         options.id = 2;
-    //         return options; }()),
-    // };
+    // Create two controllers
+    std::vector<std::shared_ptr<moteus::Controller>> controllers = {
+        std::make_shared<moteus::Controller>([&]()
+                                             {
+            auto options = options_common;
+            options.id = 1;
+            return options; }()),
+        std::make_shared<moteus::Controller>([&]()
+                                             {
+            auto options = options_common;
+            options.id = 2;
+            return options; }()),
+    };
 
-    // for (auto &c : controllers)
-    // {
-    //     c->SetStop();
-    // }
+    for (auto &c : controllers)
+    {
+        c->SetStop();
+    }
 
-    // moteus::PositionMode::Command cmd;
-    // cmd.kp_scale = 0.0;
-    // cmd.kd_scale = 0.0;
-    // cmd.feedforward_torque = 0.0;
+    moteus::PositionMode::Command cmd;
+    cmd.kp_scale = 0.0;
+    cmd.kd_scale = 0.0;
+    cmd.feedforward_torque = 0.0;
 
-    // const double MAX_TORQUE = 0.2;
-    // double torque_command[2] = {};
+    const double MAX_TORQUE = 0.2;
+    double torque_command[2] = {};
 
-    // std::vector<moteus::CanFdFrame> send_frames;
-    // std::vector<moteus::CanFdFrame> receive_frames;
+    std::vector<moteus::CanFdFrame> send_frames;
+    std::vector<moteus::CanFdFrame> receive_frames;
 
-    // // Manually input torque commands
-    // torque_command[0] = inputAndLimitTorque("motor 1", MAX_TORQUE);
-    // torque_command[1] = inputAndLimitTorque("motor 2", MAX_TORQUE);
+    // Manually input torque commands
+    torque_command[0] = inputAndLimitTorque("motor 1", MAX_TORQUE);
+    torque_command[1] = inputAndLimitTorque("motor 2", MAX_TORQUE);
 
-    // std::cout << "Torque command for motor 1: " << torque_command[0] << "Nm" << std::endl;
-    // std::cout << "Torque command for motor 2: " << torque_command[1] << "Nm" << std::endl;
-    // std::cout << "Press Ctrl+C to Stop Test" << std::endl;
+    std::cout << "Torque command for motor 1: " << torque_command[0] << "Nm" << std::endl;
+    std::cout << "Torque command for motor 2: " << torque_command[1] << "Nm" << std::endl;
+    std::cout << "Press Ctrl+C to Stop Test" << std::endl;
 
-    // // Main loop
-    // while (!ctrl_c_pressed)
-    // {
-    //     ::usleep(10);
+    // Main loop
+    while (!ctrl_c_pressed)
+    {
+        ::usleep(10);
 
-    //     send_frames.clear();
-    //     receive_frames.clear();
+        send_frames.clear();
+        receive_frames.clear();
 
-    //     for (size_t i = 0; i < controllers.size(); i++)
-    //     {
-    //         cmd.feedforward_torque = torque_command[i];
-    //         send_frames.push_back(controllers[i]->MakePosition(cmd));
-    //     }
+        for (size_t i = 0; i < controllers.size(); i++)
+        {
+            cmd.feedforward_torque = torque_command[i];
+            send_frames.push_back(controllers[i]->MakePosition(cmd));
+        }
 
-    //     // Send frames
-    //     transport->BlockingCycle(&send_frames[0], send_frames.size(), &receive_frames);
-    // }
+        // Send frames
+        transport->BlockingCycle(&send_frames[0], send_frames.size(), &receive_frames);
+    }
 
-    // std::cout << "Entering fault mode!" << std::endl;
+    std::cout << "Entering fault mode!" << std::endl;
 
-    // ::usleep(50000);
+    ::usleep(50000);
 
-    // for (auto &c : controllers)
-    // {
-    //     c->SetStop();
-    // }
+    for (auto &c : controllers)
+    {
+        c->SetStop();
+    }
 
     double scaleWeight;
     std::cout << "Enter scale reading (grams): ";
