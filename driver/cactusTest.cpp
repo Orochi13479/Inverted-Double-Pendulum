22a27c9fe819ef93d4e5e401c43eb9266b6de5de
Matthew <matthew13479@gmail.com>
2024-04-28 16:07:26 +1000

    Increasing latency

--- driver/cactusTest.cpp
+++ driver/cactusTest.cpp
@@ -136,7 +136,7 @@ int main(int argc, char **argv)
     app.Start();
     while (!cactus_rt::HasTerminationSignalBeenReceived())
     {
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
     }
 
     app.RequestStop();
