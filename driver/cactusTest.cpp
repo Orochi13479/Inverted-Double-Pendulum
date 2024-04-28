edd7a0c0b84b89dfb7eabb00c98e61bf52c254a1
Matthew <matthew13479@gmail.com>
2024-04-28 18:33:05 +1000

    Cleaning up Cactus RT

--- driver/cactusTest.cpp
+++ driver/cactusTest.cpp
@@ -6,9 +6,6 @@
 #include <chrono>
 #include <signal.h>
 
-using namespace cactus_rt;
-using namespace mjbots;
-
 // Global flag for indicating if Ctrl+C was pressed
 volatile sig_atomic_t ctrl_c_pressed = 0;
 
@@ -38,20 +35,20 @@ double inputAndLimitTorque(const std::string &motor_name, double max_torque)
     return torque_command;
 }
 
-class MotorControlThread : public CyclicThread
+class MotorControlThread : public cactus_rt::CyclicThread
 {
 private:
-    std::vector<std::shared_ptr<moteus::Controller>> controllers_;
-    std::shared_ptr<moteus::Transport> transport_;
-    moteus::PositionMode::Command cmd_;
+    std::vector<std::shared_ptr<mjbots::moteus::Controller>> controllers_;
+    std::shared_ptr<mjbots::moteus::Transport> transport_;
+    mjbots::moteus::PositionMode::Command cmd_;
     std::vector<double> torque_commands_;
     std::vector<long long> loop_durations_;
 
 public:
-    MotorControlThread(const char *name, CyclicThreadConfig config,
-                       std::vector<std::shared_ptr<moteus::Controller>> controllers,
+    MotorControlThread(const char *name, cactus_rt::CyclicThreadConfig config,
+                       std::vector<std::shared_ptr<mjbots::moteus::Controller>> controllers,
                        std::vector<double> torque_commands,
-                       std::shared_ptr<moteus::Transport> transport)
+                       std::shared_ptr<mjbots::moteus::Transport> transport)
         : CyclicThread(name, config), controllers_(controllers), torque_commands_(torque_commands), transport_(transport)
     {
         cmd_.kp_scale = 0.0;
@@ -73,8 +70,8 @@ protected:
     {
         auto start_time = std::chrono::steady_clock::now(); // Start timing
 
-        std::vector<moteus::CanFdFrame> send_frames;
-        std::vector<moteus::CanFdFrame> receive_frames;
+        std::vector<mjbots::moteus::CanFdFrame> send_frames;
+        std::vector<mjbots::moteus::CanFdFrame> receive_frames;
 
         for (size_t i = 0; i < controllers_.size(); i++)
         {
@@ -99,34 +96,34 @@ int main(int argc, char **argv)
     std::signal(SIGINT, signalHandler);
 
     // Real-time thread configuration
-    CyclicThreadConfig config;
+    cactus_rt::CyclicThreadConfig config;
     config.period_ns = 2'000'000; // Target Time in ns
     config.SetFifoScheduler(98);  // Priority 0-100
 
     // Set up controllers and transport
-    moteus::Controller::DefaultArgProcess(argc, argv);
-    auto transport = moteus::Controller::MakeSingletonTransport({});
+    mjbots::moteus::Controller::DefaultArgProcess(argc, argv);
+    auto transport = mjbots::moteus::Controller::MakeSingletonTransport({});
 
     // Options for setting up controllers
-    moteus::Controller::Options options_common;
+    mjbots::moteus::Controller::Options options_common;
 
     // Set position format
     auto &pf = options_common.position_format;
-    pf.position = moteus::kIgnore;
-    pf.velocity = moteus::kIgnore;
-    pf.feedforward_torque = moteus::kFloat;
-    pf.kp_scale = moteus::kInt8;
-    pf.kd_scale = moteus::kInt8;
+    pf.position = mjbots::moteus::kIgnore;
+    pf.velocity = mjbots::moteus::kIgnore;
+    pf.feedforward_torque = mjbots::moteus::kFloat;
+    pf.kp_scale = mjbots::moteus::kInt8;
+    pf.kd_scale = mjbots::moteus::kInt8;
 
     // Create two controllers
-    std::vector<std::shared_ptr<moteus::Controller>> controllers = {
-        std::make_shared<moteus::Controller>([&]()
-                                             {
+    std::vector<std::shared_ptr<mjbots::moteus::Controller>> controllers = {
+        std::make_shared<mjbots::moteus::Controller>([&]()
+                                                     {
                                                  auto options = options_common;
                                                  options.id = 1;
                                                  return options; }()),
-        std::make_shared<moteus::Controller>([&]()
-                                             {
+        std::make_shared<mjbots::moteus::Controller>([&]()
+                                                     {
                                                  auto options = options_common;
                                                  options.id = 2;
                                                  return options; }()),
@@ -146,9 +143,8 @@ int main(int argc, char **argv)
     auto motor_thread = std::make_shared<MotorControlThread>(
         "MotorControlThread", config, controllers, torque_commands, transport);
 
-    App app;
+    cactus_rt::App app;
     app.RegisterThread(motor_thread);
-    SetUpTerminationSignalHandler();
 
     std::cout << "Testing RT loop until CTRL+C\n";
 
