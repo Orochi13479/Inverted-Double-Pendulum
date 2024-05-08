dc31b133c270ddf57fa69ce38897cefe3c8f9a52
Matthew <matthew13479@gmail.com>
2024-05-08 14:53:28 +1000

    Fixing filepath

--- control/csvReader.cpp
+++ control/csvReader.cpp
@@ -13,13 +13,15 @@ std::vector<float> q1_dot;
 std::vector<float> q2;
 std::vector<float> q2_dot;
 
-void readCSV(const std::string& filename){
-    std::string filepath = "/home/student/git/Inverted-Double-Pendulum/trajGen/" + filename;
+void readCSV(const std::string &filename)
+{
+    std::string filepath = "../trajGen/" + filename;
 
     // Open the file
     std::ifstream file(filepath);
 
-    if (!file.is_open()) {
+    if (!file.is_open())
+    {
         throw std::runtime_error("Error: Unable to open file " + filepath);
     }
 
@@ -27,14 +29,14 @@ void readCSV(const std::string& filename){
     std::string line;
     std::getline(file, line);
 
-
-
     // Read and process the CSV data
-    while (std::getline(file, line)) {
+    while (std::getline(file, line))
+    {
         std::istringstream iss(line);
         float t, q1_val, q1_dot_val, q2_val, q2_dot_val;
         char comma;
-        if (iss >> t >> comma >> q1_val >> comma >> q1_dot_val >> comma >> q2_val >> comma >> q2_dot_val) {
+        if (iss >> t >> comma >> q1_val >> comma >> q1_dot_val >> comma >> q2_val >> comma >> q2_dot_val)
+        {
             // Add data to arrays
             timestamp.push_back(t);
             q1.push_back(q1_val);
@@ -45,15 +47,16 @@ void readCSV(const std::string& filename){
     }
 }
 
-int main() {
+int main()
+{
     // Specify the full path to the CSV file
     std::string filename = "Hand_Desgined_traj_gen_draft1.csv";
 
     readCSV(filename);
-   
 
     // Print the data (for testing)
-    for (std::size_t i = 0; i < timestamp.size(); ++i) {
+    for (std::size_t i = 0; i < timestamp.size(); ++i)
+    {
         std::cout << "timestamp: " << timestamp[i] << " ";
         std::cout << "q1: " << q1[i] << " ";
         std::cout << "q1_dot: " << q1_dot[i] << " ";
