d6d32a1f953dc71e3815b5e2c839b9a3a8bc844a
Matthew <matthew13479@gmail.com>
2024-05-22 13:50:54 +1000

    main

--- main.cpp
+++ main.cpp
@@ -188,7 +188,7 @@ int main(int argc, char **argv)
     // Signal handling setup
     std::signal(SIGINT, signalHandler);
     // Specify the full path to the CSV file
-    std::string filename = "../trajGen/RTTestTraj.csv";
+    std::string filename = "../trajGen/trajectory_data.csv";
 
     std::vector<std::vector<float>> data = readCSV(filename);
 
