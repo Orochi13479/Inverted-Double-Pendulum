9da4dd14c344dc4cd86d77cb852ebe5659ebfbd6
emmapetraglia <emma.k.petraglia@student.uts.edu.au>
2024-05-30 12:31:17 +1000

    commit

--- main.cpp
+++ main.cpp
@@ -185,7 +185,7 @@ int main(int argc, char **argv)
     // Signal handling setup
     std::signal(SIGINT, signalHandler);
     // Specify the full path to the CSV file
-    std::string filename = "../trajGen/trajectory_data_3.csv";
+    std::string filename = "../trajGen/trajectory_data_4.csv";
 
     std::vector<std::vector<float>> data = readCSV(filename);
 
