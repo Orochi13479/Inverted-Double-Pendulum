36afabc52640f66c605c771232ca4030f28d35ef
Matthew <matthew13479@gmail.com>
2024-06-01 18:03:29 +1000

    test

--- main.cpp
+++ main.cpp
@@ -170,7 +170,7 @@ protected:
                 cmd_.accel_limit = 2;
 
                 // cmd_.stop_position = cmd_pos[i];
-                controllers_[i]->SetPositionWaitComplete(cmd_, 0.0);
+                // controllers_[i]->SetPositionWaitComplete(cmd_, 0.0);
 
                 // return true;
             }
@@ -178,13 +178,13 @@ protected:
             {
                 std::cout << "TRAJ MODE" << std::endl;
                 cmd_.feedforward_torque = torque_commands_[index_][i];
-                send_frames.push_back(controllers_[i]->MakePosition(cmd_));
-                transport_->BlockingCycle(&send_frames[0], send_frames.size(), &receive_frames);
             }
+            send_frames.push_back(controllers_[i]->MakePosition(cmd_));
         }
 
         for (auto &pair : responses_)
             pair.second = false;
+        transport_->BlockingCycle(&send_frames[0], send_frames.size(), &receive_frames);
 
         for (const auto &frame : receive_frames)
             responses_[frame.source] = true;
