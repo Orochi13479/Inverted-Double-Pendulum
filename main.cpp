e7377d64d5ccbfc2c436026aebf5b9c022a27446
Matthew <matthew13479@gmail.com>
2024-06-01 14:00:48 +1000

    Changing position

--- main.cpp
+++ main.cpp
@@ -149,8 +149,8 @@ protected:
                 // cmd_.feedforward_torque = torqueWithError[i];
                 // cmd_.feedforward_torque = std::numeric_limits<double>::quiet_NaN();
                 // cmd_.position = std::numeric_limits<double>::quiet_NaN();
-                // cmd_.velocity = std::numeric_limits<double>::quiet_NaN();
-                // cmd_.position = 0.1;
+                cmd_.velocity = 0.03;
+                cmd_.position = 0.1;
                 // std::cout << "POSITION AIM " << i << ": " << cmd_pos[i] << std::endl;
 
                 // return true;
@@ -221,7 +221,7 @@ int main(int argc, char **argv)
     // Signal handling setup
     std::signal(SIGINT, signalHandler);
     // Specify the full path to the CSV file
-    std::string filename = "../trajGen/RTTestTraj.csv";
+    std::string filename = "../trajGen/TEST_DLO.csv";
 
     std::vector<std::vector<float>> data = readCSV(filename);
 
