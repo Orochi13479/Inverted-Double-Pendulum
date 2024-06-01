2a04f83e51fa1b17b1a513bfe0156e5933c5220d
Matthew <matthew13479@gmail.com>
2024-06-01 14:59:55 +1000

    Changing position

--- main.cpp
+++ main.cpp
@@ -133,7 +133,7 @@ protected:
         const std::vector<double> &last_torque_command = torque_commands_.back();
         std::vector<double> torque_diff = {TorqueError(last_torque_command[0], v1.torque), TorqueError(last_torque_command[1], v2.torque)};
 
-        printf("MODE: %2d/%2d  POSITION: %6.3f/%6.3f  TORQUE: %6.3f/%6.3f  TORQUE ERROR: %6.3f/%6.3f  TEMP: %4.1f/%4.1f  VELOCITY: %6.3f/%6.3f FAULTS: %6.3f/%6.3f\r",
+        printf("MODE: %2d/%2d  POSITION: %6.3f/%6.3f  TORQUE: %6.3f/%6.3f  TORQUE ERROR: %6.3f/%6.3f  TEMP: %4.1f/%4.1f  VELOCITY: %6.3f/%6.3f FAULTS: %2d/%2d\r",
                static_cast<int>(v1.mode), static_cast<int>(v2.mode),
                v1.position, v2.position,
                v1.torque, v2.torque, torque_diff[0], torque_diff[1],
