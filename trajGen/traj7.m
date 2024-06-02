2b0f887bb653bd087b116c5c73fd13495bdee19f
emmapetraglia <emma.k.petraglia@student.uts.edu.au>
2024-06-02 21:12:18 +1000

    commit

--- trajGen/traj7.m
+++ trajGen/traj7.m
@@ -127,7 +127,7 @@ end
 % Create CSV file of Trajectory Generation Data
 
 % Define the filename
-filename = 'trajectory_data_11.csv';
+filename = 'trajectory_data_12.csv';
 
 % Transpose each variable and concatenate them into a single matrix
 data = [t_sim(:), q1_sim(:), q1_dot_sim(:), q1_dot_dot_sim(:), tau1(:), q2_sim(:), q2_dot_sim(:), q2_dot_dot_sim(:), tau2(:)];
