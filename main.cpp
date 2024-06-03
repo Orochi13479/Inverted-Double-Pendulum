90f82fe7441ed09b661d58d3b37d758d8d2c183d
Matthew <matthew13479@gmail.com>
2024-06-03 17:54:10 +1000

    Testing

--- main.cpp
+++ main.cpp
@@ -210,7 +210,7 @@ int main(int argc, char **argv)
 
     // Real-time thread configuration
     cactus_rt::CyclicThreadConfig config;
-    config.period_ns = 2'860'000; // Target Time in ns
+    config.period_ns = 3'000'000; // Target Time in ns
     config.SetFifoScheduler(98);  // Priority 0-100
 
     // Set up controllers and transport
