f0d48fee2eebb1ec06eae7e9c21c7cf2a573918c
Matthew <matthew13479@gmail.com>
2024-06-03 11:49:31 +1000

    Testing

--- trajRecord.cpp
+++ trajRecord.cpp
@@ -46,10 +46,13 @@ int main(int argc, char **argv)
 
     // Open a CSV file for writing
     std::ofstream csv_file("motor_data.csv");
-    csv_file << "Time,Mode1,Mode2,Position1,Position2,Velocity1,Velocity2,Torque1,Torque2\n";
+    csv_file << "Time,q1,q1_dot,q1_dot_dot,tau1,q2,q2_dot,q2_dot_dot,tau2\n";
 
     // Get the start time
     auto start = std::chrono::steady_clock::now();
+
+    std::cout << "Press Enter to start recording and CTRL+C to stop";
+    std::cin.get();
 
     // Main loop
     while (!ctrl_c_pressed)
@@ -62,20 +65,18 @@ int main(int argc, char **argv)
 
         // Get the current time in milliseconds since start
         auto now = std::chrono::steady_clock::now();
-        auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(now - start).count();
+        auto elapsed = std::chrono::duration_cast<std::chrono::seconds>(now - start).count();
 
         // Write data to the CSV file
         csv_file << elapsed << ","
-                 << static_cast<int>(v1.mode) << "," << static_cast<int>(v2.mode) << ","
-                 << v1.position << "," << v2.position << ","
-                 << v1.velocity << "," << v2.velocity << ","
-                 << v1.torque << "," << v2.torque << "\n";
+                 << v1.position << "," << v1.velocity << "," << "0.0" << "," << v1.torque
+                 << v2.position << "," << v2.velocity << "," << "0.0" << "," << v2.torque << "\n";
     }
 
     // Close the CSV file
     csv_file.close();
 
-    std::cout << "Entering fault mode!" << std::endl;
+    std::cout << "File Written" << std::endl;
 
     ::usleep(50000);
 
