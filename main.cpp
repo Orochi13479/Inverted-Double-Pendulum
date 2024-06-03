519e67c3b30cf3b85c8ae71952d2a9397a3080ff
Matthew <matthew13479@gmail.com>
2024-06-03 15:04:30 +1000

    Testing

--- main.cpp
+++ main.cpp
@@ -276,7 +276,7 @@ int main(int argc, char **argv)
     std::vector<int> time_intervals;
     for (size_t i = 1; i < data.size(); ++i) // Start from the second element
     {
-        time_intervals.push_back((data[i][0] * 150) - (data[i - 1][0] * 150));
+        time_intervals.push_back((data[i][0] * 100) - (data[i - 1][0] * 100));
     }
 
     std::cout << "time_intervalssize " << time_intervals.size() << std::endl;
