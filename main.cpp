a2e5840b581afbaba1e603ba88160e77b7c0f39c
Matthew <matthew13479@gmail.com>
2024-06-03 15:26:56 +1000

    Testing

--- main.cpp
+++ main.cpp
@@ -117,8 +117,8 @@ protected:
         std::vector<double> cmd_kp = {10.0, 1};
         std::vector<double> cmd_kd = {5.0, 0.5};
 
-        std::vector<double> cmd_pos = {0.5, 0.0};
-        std::vector<double> cmd_pos_backup = {0.6, 0.0};
+        std::vector<double> cmd_pos = {0.7, 0.0};
+        std::vector<double> cmd_pos_backup = {0.5, 0.0};
 
         auto maybe_servo1 = controllers_[0]->SetQuery();
         auto maybe_servo2 = controllers_[1]->SetQuery();
