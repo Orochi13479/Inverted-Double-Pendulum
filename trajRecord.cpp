86313c870a79cec5a52b20e1fa935c18a6c71f92
Matthew <matthew13479@gmail.com>
2024-06-03 12:57:40 +1000

    Testing

--- trajRecord.cpp
+++ trajRecord.cpp
@@ -45,8 +45,8 @@ int main(int argc, char **argv)
     };
 
     // Open a CSV file for writing
-    std::ofstream csv_file("../trajGen/motor_data.csv");
-    csv_file << "Time,q1,q1_dot,q1_dot_dot,tau1,q2,q2_dot,q2_dot_dot,tau2\n";
+    // std::ofstream csv_file("../trajGen/motor_data.csv");
+    // csv_file << "Time,q1,q1_dot,q1_dot_dot,tau1,q2,q2_dot,q2_dot_dot,tau2\n";
 
     std::cout << "Press Enter to start recording and CTRL+C to stop";
     std::cin.get();
@@ -65,19 +65,19 @@ int main(int argc, char **argv)
         // Get the current time in milliseconds since start
         auto now = std::chrono::steady_clock::now();
         auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(now - start).count();
-        // printf("POSITION: %6.3f/%6.3f  VELOCITY: %6.3f/%6.3f TORQUE: %6.3f/%6.3f\r",
-        //        v1.position, v2.position, v1.velocity, v2.velocity,
-        //        v1.torque, v2.torque);
-        // fflush(stdout);
+        printf("POSITION: %6.3f/%6.3f  VELOCITY: %6.3f/%6.3f TORQUE: %6.3f/%6.3f\r",
+               v1.position, v2.position, v1.velocity, v2.velocity,
+               v1.torque, v2.torque);
+        fflush(stdout);
 
         // Write data to the CSV file
-        csv_file << elapsed * 0.001 << ","
-                 << v1.position << "," << v1.velocity << "," << "0.0" << "," << v1.torque << ","
-                 << v2.position << "," << v2.velocity << "," << "0.0" << "," << v2.torque << "\n";
+        // csv_file << elapsed * 0.001 << ","
+        //          << v1.position << "," << v1.velocity << "," << "0.0" << "," << v1.torque << ","
+        //          << v2.position << "," << v2.velocity << "," << "0.0" << "," << v2.torque << "\n";
     }
 
     // Close the CSV file
-    csv_file.close();
+    // csv_file.close();
 
     std::cout << "File Written" << std::endl;
 
